9b46866484b589d233ce644486309abef4fcad39
Adam Paterson <hello@adampaterson.co.uk>
2014-10-09 13:06:32 +0100

    PSR-2 Standards, ignoring bootstrap though

--- src/Application.php
+++ src/Application.php
@@ -1,18 +1,35 @@
 <?php
+/**
+ * Puppet Config CLI Application for generating YAML config file.
+ *
+ * PHP Version 5
+ *
+ * @category Application
+ * @package  Juno
+ * @author   Adam Paterson <hello@adampaterson.co.uk>
+ * @license  MIT https://github.com/juno-media/puppet-config/blob/master/LICENSE
+ * @link     https://github.com/juno-media/puppet-config
+ */
 namespace Juno;
 
 use Symfony\Component\Console\Application as BaseApplication;
 
 /**
  * Class Application
- * @package Juno
- * @author Adam Paterson <hello@adampaterson.co.uk>
+ *
+ * @category Application
+ * @package  Juno
+ * @author   Adam Paterson <hello@adampaterson.co.uk>
+ * @license  MIT https://github.com/juno-media/puppet-config/blob/master/LICENSE
+ * @link     Symfony\Component\Console\Application
  */
 class Application extends BaseApplication
 {
     /**
      * Juno Media logo
      *
+     * @category Application
+     * @package Juno
      * @var
      */
     protected static $logo = "
@@ -25,13 +42,18 @@ class Application extends BaseApplication
     ";
 
     /**
-     * Create output formatter and set application name and version.
+     * Create output formatter and set application name and version
      *
-     * @param string $name
-     * @param string $version
+     * @param string $name    Application name
+     * @param string $version Application version
+     *
+     * @category Application
+     * @package Juno
      */
-    public function __construct($name = 'Puppet Config Generator', $version = '1.0.0')
-    {
+    public function __construct(
+        $name = 'Puppet Config Generator',
+        $version = '1.0.0'
+    ) {
         return parent::__construct($name, $version);
     }
 
